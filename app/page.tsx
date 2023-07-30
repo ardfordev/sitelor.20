df5bd652cc49d011b194896beb8898ec04605f19
ardfordev <99476410+ardfordev@users.noreply.github.com>
2023-07-30 12:30:20 +0700

    add HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
-import { Brand } from '@/components/brand'
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import eastjavaMap from "@/public/map.png"
+import { Brand } from '@/components/brand'
+import { Button } from '@/components/ui/button'
+import { LogIn } from 'lucide-react'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 export default function HomePage() {
   return (
     <div>
       <Brand/>
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          SITELOR.20
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground">
+          Sistem Informasi Technical Losses Realtime 20 KV
+        </p>
+      </div>
+      <Link href='/login'>
+        <Button className='w-32' variant="outline">
+          <LogIn className="mr-2 h-4 w-4" />
+          Login
+        </Button>
+      </Link>
+      <div className="w-full">
+          <AspectRatio ratio={16 / 9}>
+            <Image
+              src={eastjavaMap}
+              alt="UP2D JATIM"
+              className="rounded-md object-cover"
+              priority
+            />
+          </AspectRatio>
+        </div>
+    </section>
     </div>
   )
 }
---
+++ components/site-footer.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from "./mode-toggle"
+
+export function SiteFooter() {
+  return (
+    <footer className="py-6 md:border-t md:px-8 md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-12 md:flex-row">
+        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Unit Pelaksana Pengatur Distribusi JATIM
+        </p>
+        <ModeToggle/>
+      </div>
+    </footer>
+  )
+}
